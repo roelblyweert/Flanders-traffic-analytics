3b4e527f945fbc67019f952c1a155d82ded34683
Roel Blyweert <roel.blyweert@smtware.com>
2017-07-02 15:04:35 +0200

    Adding the inventory import as a modular input with initialization working now.

---
+++ RB_SH_trafficflanders_backend/README/inputs.conf.spec
@@ -0,0 +1,9 @@
+[traffic_inv_kvstore://default]
+*Traffic Flanders to KV store
+
+fetch_url = <value>
+*This is the URL needed to make the actual HTTP call and fetch the inventory data.
+
+dest_collection = <value>
+*This is the destination KV store collection name where the inventory data needs to be stored.
+
--- RB_SH_trafficflanders_backend/bin/traffic_inv_kvstore.py
+++ RB_SH_trafficflanders_backend/bin/traffic_inv_kvstore.py
@@ -2,6 +2,24 @@
 # Author: Roel Blyweert <blyweert.roel@gmail.com>
 # Copyright: Roel Blyweert
 
+import sys, datetime, json, os , os.path as op, time, urllib2
+import ConfigParser
+import json
+import urllib2
+import urllib
+import re
+import logging
+
+# Import of add-on specific modules
+from splunkapi.splunkclient import Splunkclient
+from splunkapi.kv_client import KVClient
+
+# Import ElementTree package for walking over XML trees
+import elementtree.ElementTree as etree
+
+# import the Splunk modular inputs related modules
+from splunklib.modularinput import *
+
 ###############################
 ####### Logger functions ######
 ###############################
@@ -44,7 +62,41 @@ class TrafficInvToKVStore(Script):
 		# Returns scheme.
 		scheme = Scheme("Flanders Traffic inventory to KV store modular input")
 		scheme.description = "Captures inventory information from the traffic related sensors in Flanders via its open data platform and pushes this to the KV store."
-   
+		
+		# add the fetch URL as an argument
+		endpoint_argument = Argument("fetch_url")
+		endpoint_argument.title = "Remote inventory fetching URL"
+		endpoint_argument.data_type = Argument.data_type_string
+		endpoint_argument.description = "This URL is the source of the XML data containing the sensor inventory data information for all measuring points."
+		endpoint_argument.require_on_create = True
+		scheme.add_argument(endpoint_argument)
+		
+		# add the destination KV store collection as an argument
+		hostname_argument = Argument("dest_collection")
+		hostname_argument.title = "Destination KV store collection"
+		hostname_argument.data_type = Argument.data_type_string
+		hostname_argument.description = "This is the destination KV store collection name where the sensor inventory data will be stored."
+		hostname_argument.require_on_create = True
+		scheme.add_argument(hostname_argument)
+		
+		return scheme
+	
+	# subroutine to validate the inputs of the modular input
+	def validate_input(self, validation_definition):
+		logger = setup_logging()
+		logger.info("validate_inputs: " + time.strftime("%d-%m-%Y %H:%M:%S"))
+		
+		# get the parameters
+		fetch_url = validation_definition.parameters["fetch_url"]
+		dest_collection = validation_definition.parameters["dest_collection"]
+		
+	def stream_events(self, inputs, ew):
+		# Splunk Enterprise calls the modular input, streams XML describing the inputs to stdin,
+		# and waits for XML on stdout describing events. The stdout is the data that gets indexes in Splunk.
+		# The stin also includes a session_key that can be used to communicate with the Splunk REST API.
+		logger = setup_logging()
+		logger.debug("stream_events: " + time.strftime("%d-%m-%Y %H:%M:%S"))
+
 # main entry point of the modular input
 if __name__ == "__main__":
     sys.exit(TrafficInvToKVStore().run(sys.argv))
